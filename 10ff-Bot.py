573b379c311ecda61c9353f0b1d1b0fe5c82b420
Mikey <harshmetaexploit90@gmail.com>
2022-02-24 14:23:56 +0530

    added login and typing speed functionality

--- 10ff-Bot.py
+++ 10ff-Bot.py
@@ -1,103 +1,153 @@
 
-import sys
-import pyautogui, time
+import argparse
+from argparse import RawTextHelpFormatter
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-path = r"your/path/to/chromedriver"
+path = r"C:\Users\harsh\chromedriver_win32\chromedriver.exe"
 
 def main():
-    if(len(sys.argv) >= 2):
-        if(sys.argv[1] == "-h" or sys.argv[1] == "--help"):
-            print(f"\nUsage: {sys.argv[0]} --<mode>\
-                \n\nMode:\n\t--normtest = to enter the normal typing test.\
-                \n\t--advtest = to enter the advanced typing test.\
-                \n\t--compete = to compete with other players.".expandtabs(4))
-        else:
-            global driver
-            driver = webdriver.Chrome(path)
-            if(sys.argv[1] == "--normtest"): typingTest()
-            elif(sys.argv[1] == "--advtest"): advTypingTest()
-            elif(sys.argv[1] == "--compete"): multiplayerTypingTest()
-            else: sys.exit(1)
+    parser = argparse.ArgumentParser(description="A super fast typer bot for 10fastfingers.com", formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-l', '--login', metavar='', help='plays with user specified profile.\nFormat= --login <email-id>&passwd')
+    parser.add_argument('-t', '--tempo', metavar='', help='plays with user specified speed.\nFormat= --tempo <int>x\n\t  --tempo max')
+    mode = parser.add_argument_group("typing mode arguments")
+    mode.add_argument('-n', '--normtest', action='store_true', help='enters the normal typing test')
+    mode.add_argument('-a', '--advtest', action='store_true', help='enters the advanced typing test')
+    mode.add_argument('-c', '--compete', action='store_true', help='enters the competitive mode')
 
+    args = parser.parse_args()
+    
+    global driver
+    driver = webdriver.Chrome(path)
+    
+    if (bool(args.login)):
+
+        loginArr = args.login.split('>&')
+        login_(loginArr[0][1:], loginArr[1])
+        loggedIn = True
     else:
-        print(f"\nUsage: {sys.argv[0]} --<mode>\
-            \nFor more, check help section:\
-            \n\t{sys.argv[0]} --help 'or' -h".expandtabs(4))
 
+        loggedIn = False
+        driver.get("https://10fastfingers.com")
+        allow = locator(By.ID,'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection')
+        time.sleep(2)
+
+        if allow.text == 'Allow selection':
+            allow.click()
+
+    if(bool(args.tempo)):
+        speed = args.tempo
+    else:
+        speed = "1x"
+
+    argument = arg_validator(args.normtest, args.advtest, args.compete)
+    if argument == '-n' or argument == '-a':
+        typeIt(argument, speed, loggedIn)
+    elif argument == '-c':
+        multiplayerTypingTest()
+    elif argument == -1:
+        parser.error("atleast select one mode.")  
+    else:
+        parser.error("select only one mode.")  
+    # print(login.split('>&')[0][1:])
+
+def arg_validator(f_arg, s_arg, th_arg):
+    if(f_arg and not s_arg and not th_arg):
+        return "-n"
+    elif(s_arg and not f_arg and not th_arg):
+        return "-a"
+    elif(th_arg and not s_arg and not f_arg):
+        return "-c"
+    elif(not th_arg and not s_arg and not f_arg):
+        return -1
+    else: return 0
 
 def locator(selector, reference):
     outcome = WebDriverWait(driver, 10).until(EC.presence_of_element_located((selector, reference)))
     return outcome
 
-def typingTest():
-    driver.get('https://10fastfingers.com/typing-test/english')
+def login_(email, password):
 
+    driver.get("https://10fastfingers.com/login")
     allow = locator(By.ID,'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection')
     time.sleep(2)
 
     if allow.text == 'Allow selection':
         allow.click()
+    
+    email_box = locator(By.ID, "UserEmail")
+    time.sleep(1)
+    email_box.click()
+    email_box.send_keys(email)
+
+    password_box = locator(By.ID, "UserPassword")
+    time.sleep(1)
+    password_box.click()
+    password_box.send_keys(password)
+    password_box.send_keys(Keys.ENTER)
+
+
+def typeIt(arg, speed, auth):
+
+    if arg == '-n':
+        if(auth):
+            pass
+        else:
+            norSec = locator(By.XPATH, '//*[@id="sidebar"]/div/a[1]')
+            time.sleep(0.5)
+            norSec.click()
+    elif arg == '-a':
+        if(auth):
+            driver.get('https://10fastfingers.com/advanced-typing-test/english')
+        else:
+            advSec = locator(By.XPATH, '//*[@id="sidebar"]/div/a[2]')
+            time.sleep(0.5)
+            advSec.click()
 
     typerInput = locator(By.ID,'inputfield')
     time.sleep(1)
     typerInput.click()
 
+    nSpeed = 0.1
+    i=1
+    timer = 60
     try:
-        i=1
-        timer = 60
         while timer > 0:                                                                       
             timer_str = locator(By.XPATH,'//*[@id="timer"]').text
-            if(int(timer_str[2:]) == 0):
+            if(timer_str == "1:00"):
                 timer = 60
-            else: timer = int(timer_str[2:])+1
+            else: 
+                timer = int(timer_str[2:])
+                print(timer)
             word = locator(By.XPATH,f'//*[@id="row1"]/span[{i}]').text
-            typerInput.send_keys(word)
+            for char in word:
+                typerInput.send_keys(char)
+                if(speed ==  "max"):
+                    pass
+                else:
+                    time.sleep(nSpeed/int(speed[:-1]))
             typerInput.send_keys(Keys.SPACE)
             i+=1
+
     except:
-        time.sleep(timer-10)
-    pyautogui.press('enter')
+        if timer == 0:
+            wpm = locator(By.XPATH,'//*[@id="wpm"]/strong').text
+            print("-"*30,"\n", f"{wpm[:-4]} wpm")
+            driver.quit()
+        else:
+            print(timer)
+            time.sleep(timer)
+            alert_obj = driver.switch_to.alert
+            alert_obj.dismiss()
+
     wpm = locator(By.XPATH,'//*[@id="wpm"]/strong').text
     print("-"*30,"\n", f"{wpm[:-4]} wpm")
     driver.quit()
-
-def advTypingTest():
     
-    driver.get('https://10fastfingers.com/advanced-typing-test/')
-
-    allow = locator(By.ID,'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection')
-    time.sleep(2)
-
-    if allow.text == 'Allow selection':
-        allow.click()
-
-    typerInput = locator(By.ID,'inputfield')
-    time.sleep(1)
-    typerInput.click()
-
-    try:
-        i=1
-        timer = 60
-        while timer > 0:                                                                       
-            timer_str = locator(By.XPATH,'//*[@id="timer"]').text
-            if(int(timer_str[2:]) == 0):
-                timer = 60
-            else: timer = int(timer_str[2:])+1
-            word = locator(By.XPATH,f'//*[@id="row1"]/span[{i}]').text
-            typerInput.send_keys(word)
-            typerInput.send_keys(Keys.SPACE)
-            i+=1
-    except:
-        time.sleep(timer-10)
-    pyautogui.press('enter')
-    wpm = locator(By.XPATH,'//*[@id="wpm"]/strong').text
-    print("-"*30,"\n", f"{wpm[:-4]} wpm")
-    driver.quit()  
 
 def multiplayerTypingTest():
     
@@ -124,7 +174,8 @@ def multiplayerTypingTest():
             i+=1
     except:
         wpm = locator(By.XPATH, '//*[@id="game"]/div[3]/div[2]/div/div/table/tbody/tr/td[2]').text
-        print("-"*30, "\n", f"{wpm}")
+        wpm = wpm.replace(" ", "").split()
+        print("-"*30, "\n", f"{wpm[0]} WPM")
     time.sleep(2)
     driver.quit()
     
